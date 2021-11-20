5e39db22ccd7383ac874e8e7301d971e24201c4b
Gustaf Delport <gustafdelport@gmail.com>
2021-11-20 10:56:33 +0200

    Add Comments
    Added comments to the Hardware code

--- Hardware/Arduino/Arduino.ino
+++ Hardware/Arduino/Arduino.ino
@@ -2,7 +2,7 @@
 #include <dht.h>
 #include <SoftwareSerial.h>
 
-//Instances
+//Instances that is used by the Arduino
 dht DHT;
 SoftwareSerial node(2, 3); //RX | TX
 
@@ -26,6 +26,7 @@ int lightVal;
 //---------------------------------------------------------------------------
 
 void setup() {
+  //Creating the serial comunications 9600 Baud Rate
   Serial.begin(9600);
   node.begin(9600);
   delay(1000);
@@ -36,16 +37,21 @@ void setup() {
 }
 
 void loop() {
+  //Json buffer that allows 1000 bits of data to be send and received
   StaticJsonBuffer<1000> jsonBuffer;
   JsonObject& data = jsonBuffer.createObject();
   
   //Temp and Humidity
   //--------------------------------------------------------------------------
+
+  //Analog data is read from the Arduino
   float chk = DHT.read11(DHT11_PIN);
   
+  //The DHT library automatically converts the data from Analog to Human readable data 
   temp = DHT.temperature;
   humi = DHT.humidity;
 
+  //Store the data in the Json object file.
   data["temp"] = temp;
   data["humi"] = humi;
   //--------------------------------------------------------------------------
@@ -56,23 +62,29 @@ void loop() {
   //# 300~700     humid soil
   //# 700~950     in water
 
+  //Analog data is read from the Arduino
   mois = analogRead(MOIS_PIN);
+  //Store the data in the Json object file.
   data["mois"] = mois;
   
   //---------------------------------------------------------------------------
   //Light Sensor
 
+  //Analog data is read from the Arduino
   lightVal = analogRead(PRES_PIN);
+  //Store the data in the Json object file.
   data["light"] = lightVal;
   //---------------------------------------------------------------------------
-  //Sending data to ESP for server transfer
 
+  //Sending data to ESP for server transfer and printing the data for debugging purposes.
   data.printTo(node);
   data.printTo(Serial);
   Serial.println();
   jsonBuffer.clear();
   //--------------------------------------------------------------------------
   
+  //Decision: The analog data is checked and according to the calibration it decides if the water 
+  //pump needs to turn on or off
   if (mois <= 450)
   {
     digitalWrite(RELAY_PIN, HIGH);
@@ -84,6 +96,8 @@ void loop() {
   //--------------------------------------------------------------------------
   //Code based simulations (Hardware we could not afford)
 
+  //Decision: The analog data is checked and according to the calibration it decides if the fans 
+  //need to be turn on or off
   if (temp >= 30)
   {
     Serial.println("Turning on fans")
@@ -94,12 +108,14 @@ void loop() {
 
   }
 
+  //Decision: The analog data is checked and according to the calibration it decides if the lights 
+  //need to be turn on or off
   if (lightVal <= 100)
   {
-    Serial.println("Going Dark: Swithing on lights for nightime development")
+    Serial.println("Going Dark: Switching on lights for nightime development")
   }
   else{
-    Serial.println("Sun is shining: Swithing to free light power --> Sun active")
+    Serial.println("Sun is shining: Switching to free light power --> Sun active")
   }
   //--------------------------------------------------------------------------
 
--- Hardware/Node/Node.ino
+++ Hardware/Node/Node.ino
@@ -4,7 +4,7 @@
 #include <ESP8266WiFi.h>
 //--------------------------------------------
 
-//Variables
+//Variables declirations
 String passData;
 float temp;
 float humi;
@@ -12,6 +12,8 @@ float mois;
 int lightVal;
 boolean handshakeFailed=0;
 
+//This is the configuration for the Node device and with some 
+//sensitive data that is needed for it to connect to the internet and server
 char path[] = "/NodeOne";
 const char* ssid     = "Delport-WiFi 2.4GHz";
 const char* password = "Adp!001G";
@@ -21,37 +23,40 @@ const int espport= 6579;
 
 //--------------------------------------------
 
-//Instances
+//Basic Instances
 WebSocketClient webSocketClient;
-SoftwareSerial node(D2, D1); //SRX=Dpin-D2; STX-DPin-D1
+//SRX=DPin-D2; STX-DPin-D1
+SoftwareSerial node(D2, D1);
 WiFiClient client;
 
 //--------------------------------------------
-//Code
-
 void setup()
 {
-  Serial.begin(9600); //enable Serial Monitor
-  node.begin(9600); //enable Node Port
+  //enable Serial Monitor
+  Serial.begin(9600);
+  //enable Node Port
+  node.begin(9600); 
   
-  Serial.println();
-  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
   
+  //Initialize the wifi protocol according to configurations above.
   WiFi.begin(ssid, password);
   
+  //This function will run until a wifi connection is made.
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   
+  //Displays the status and IP address of the network the device connected to.
   Serial.println("");
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   delay(1000);
   
+  //Calls the WebSocket connect function to initialize a handshake with the server.
   wsconnect();
   
   while(!Serial) continue;
@@ -59,40 +64,44 @@ void setup()
   
 void loop()
 {
-  //Recieving data from Arduino
+  //Receiving data from Arduino inside a exact replica Json object and buffer 
   StaticJsonBuffer<1000> jsonBuffer;
   JsonObject& data = jsonBuffer.parseObject(node);
   
+  //Decision: This checks if the data received is correct of corrupt and the either 
+  //clears the buffer or continues.
   if (data == JsonObject::invalid())
   {
     jsonBuffer.clear();
     return;
   }
   
+  //Fill variables with data from Json Object.
   temp = data["temp"];
   humi = data["humi"];
   mois = data["mois"];
   lightVal = data["light"];
 
-  //Display data in serial to test we remove this later
-  Serial.print(temp);
-  Serial.print(",");
-  Serial.print(humi);
-  Serial.print(",");
-  Serial.print(mois);
-  Serial.print(",");
-  Serial.print(lightVal);
-  Serial.println();
+  //Display data in serial to test we remove this later 
+  //"Debugging uncomment this"
+
+  // Serial.print(temp);
+  // Serial.print(",");
+  // Serial.print(humi);
+  // Serial.print(",");
+  // Serial.print(mois);
+  // Serial.print(",");
+  // Serial.print(lightVal);
+  // Serial.println();
   
   //--------------------------------------------
 
   //Posting data to server
-  
   if (client.connected()) {    
-    //webSocketClient.getData(data);
-          
+    //Create a single line string seperated with comas to send in a format to backend server.  
     passData = (String)temp + "," + (String)humi + "," + (String)mois + "," + (String)lightVal;
-    webSocketClient.sendData(passData);//send sensor data to websocket server
+    //send sensor data to websocket server
+    webSocketClient.sendData(passData);
   }
   //--------------------------------------------
 }
@@ -103,6 +112,7 @@ void wsconnect(){
     Serial.println("Connected");
   } 
   else {
+    //If connection is refused or lost this function will constantly check until it connects.
     Serial.println("Connection failed.");
     delay(1000);  
    
@@ -117,6 +127,7 @@ void wsconnect(){
   webSocketClient.path = path;
   webSocketClient.host = host;
   
+  //We have connected successfully
   if (webSocketClient.handshake(client)) {
     Serial.println("Handshake successful");
   } 
@@ -124,6 +135,7 @@ void wsconnect(){
     Serial.println("Handshake failed.");
     delay(4000);  
    
+    //After a fail we reset the entire device and try again until successful
     if(handshakeFailed){
       handshakeFailed=0;
       ESP.restart();
